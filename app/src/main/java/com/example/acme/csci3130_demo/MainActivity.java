3945565d404f3c4b4e132b8baf4db0e51cf77f30
hoyya <padraecr@gmail.com>
2018-01-23 23:40:58 -0400

    hopefully this works

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,31 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView viewbox;
+    Button button;
+    EditText inputbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputbox = (EditText) findViewById(R.id.inputbox);
+        button = (Button) findViewById(R.id.button);
+        viewbox = (TextView) findViewById(R.id.viewbox);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewbox.setText(inputbox.getText());
+            }
+        });
     }
 }
